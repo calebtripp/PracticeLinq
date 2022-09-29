de8666a2524d8e0001f860af857cb88fbf0b17cc
calebtripp <calebtripp@outlook.com>
2022-09-29 18:08:03 -0400

    Progress

---
+++ PracticeLinq/PracticeLinq/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+
+var vGList = new List<string>();
+
+vGList.Add("Command and Conquer Tiberian Sun");
+vGList.Add("Warcraft 3");
+vGList.Add("Halo");
+vGList.Add("Tomorrow Never Dies");
+vGList.Add("Delta Force");
+vGList.Add("Contra");
+vGList.Add("Super Mario Bros");
+vGList.Add("Twisted Metal");
+
+
+
+
+//Order the list of games by length of the game name.
+//Use the lambda expression in this exercise as well.
+//use Method Syntax for this exercise
+
